fc03657566c0f1c1bbb3ad16772d3e6b3d1d96ba
A1aric <rongw2@illinois.edu>
2017-12-07 15:39:31 -0600

    mp7

---
+++ src/CourseInfo.java
@@ -0,0 +1,59 @@
+
+
+public class CourseInfo {
+	private String title;
+	private int sum_students;
+	private double avg_gpa;
+	private int number;
+	private String subject;
+	private String name;
+	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getSum_students() {
+		return sum_students;
+	}
+
+	public void setSum_students(int sum_students) {
+		this.sum_students = sum_students;
+	}
+
+	public double getAvg_gpa() {
+		return avg_gpa;
+	}
+
+	public void setAvg_gpa(double avg_gpa) {
+		this.avg_gpa = avg_gpa;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+}
---
+++ src/GetGPA.java
@@ -0,0 +1,44 @@
+	import java.io.BufferedReader;
+	import java.io.File;
+	import java.io.FileNotFoundException;
+	import java.io.FileReader;
+	import java.io.IOException;
+	import java.io.Reader;
+	import java.lang.reflect.Type;
+	import java.util.ArrayList;
+	import java.util.List;
+
+	import javax.swing.text.html.HTMLDocument.Iterator;
+
+	import com.google.gson.Gson;
+	import com.google.gson.JsonArray;
+	import com.google.gson.JsonDeserializer;
+	import com.google.gson.JsonElement;
+	import com.google.gson.stream.JsonReader;
+	import com.google.gson.JsonParser;
+	import com.google.gson.reflect.TypeToken;
+
+	public class GetGPA {
+		public static void main(String[] args) throws FileNotFoundException {
+			System.out.println("Input what course you want to know");
+			
+			readDataFromJson("CS 173");
+		}
+
+		public static void readDataFromJson(String s) throws FileNotFoundException {
+
+			JsonReader file = new JsonReader(new FileReader("/Users/ric/Desktop/generic.json"));
+			Gson gson = new Gson();
+			List<CourseInfo> rs = new ArrayList<CourseInfo>();
+			Type type = new TypeToken<ArrayList<CourseInfo>>() {
+			}.getType();
+			rs = gson.fromJson(file, type);
+			for (CourseInfo a : rs) {
+				if (a.getName().equals(s)) {
+					System.out.println(a.getName() + " " + a.getSum_students());
+				}
+			}
+		}
+
+	}
+
